79892b86d7d5f1b3ba788b8c6beac66c74905ee7
JoaooGabriel <caetanojoaogabriel@gmail.com>
2021-12-04 22:22:18 -0300

    fix: add cors middleware

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config'
 import express from 'express'
+import cors from 'cors'
 import { userRouter } from './user/router'
 import { scheduleRouter } from './schedule/router'
 
 export const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.use('/v1/users/', userRouter)
 app.use('/v1/schedules/', scheduleRouter)
